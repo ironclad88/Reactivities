22f707ed00eb727683115958409b798c95ae5c8c
ironclad88 <sebastian.thernstrom@gmail.com>
2023-10-31 07:56:11 +0100

    Security headers added

--- API/Program.cs
+++ API/Program.cs
@@ -12,7 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(opt => {
+builder.Services.AddControllers(opt =>
+{
     var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
     opt.Filters.Add(new AuthorizeFilter(policy));
 });
@@ -23,11 +24,34 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseXContentTypeOptions();
+app.UseReferrerPolicy(opt => opt.NoReferrer());
+app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+app.UseXfo(opt => opt.Deny());
+app.UseCsp(opt => opt
+.BlockAllMixedContent()
+.StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+.FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+.FormActions(s => s.Self())
+.FrameAncestors(s => s.Self())
+.ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com"))
+.ScriptSources(s => s.Self())
+);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    // app.UseHsts() does not work for some reason, so we create our own header for this one
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
 }
 
 app.UseCors("CorsPolicy");
